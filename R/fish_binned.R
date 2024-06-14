248de398f341f0d04903ecc421b48c9da2c361f5
Jpomz <jfpomeranz@gmail.com>
2024-06-14 12:35:11 -0600

    added count and simulated bin data

---
+++ R/fish_binned.R
@@ -0,0 +1,16 @@
+#' Simulated binned dataset
+#'
+#' This data is simulated to represent fish data which is recorded in size "bins" in the field. Here, bins of 5g are used and the count of fish in each bin is tallied.
+#'
+#'
+#' @format ## `fish_binned`
+#' A data frame with 58 rows and 4 columns:
+#' \describe{
+#'   \item{binMid}{The mid point of the bin}
+#'   \item{binMin}{The minimum edge of a bin}
+#'   \item{binMax}{The maximum edge of a bin}
+#'   \item{n}{count of individuals in that bin}
+#'   ...
+#' }
+#' @source Simulated
+"fish_binned"
